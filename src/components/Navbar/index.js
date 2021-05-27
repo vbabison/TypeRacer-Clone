1a364486b161b79097606247d48e64a11df7a074
Babison Vilvarajah <officialbookingssf@gmail.com>
2021-05-27 03:16:59 -0400

    adding nav bar css and fixing image import

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,7 +13,7 @@ const Navbar = () => {
     <>
       <Nav>
         <NavLink to='/'>
-          <img src={require('../../images/logo.svg')} alt='logo' />
+          <img src={require('../../images/logo.png')} alt='logo' />
         </NavLink>
         <Bars />
         <NavMenu>
