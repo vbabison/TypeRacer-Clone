60b7dd9d7f038edf2f0da54e56f7281074d3897a
Babison Vilvarajah <officialbookingssf@gmail.com>
2021-06-03 13:02:11 -0400

    Adding mongoose and schema

--- src/pages/models/Game.js
+++ src/pages/models/Game.js
@@ -5,5 +5,18 @@ const PlayerSchema = new mongoose.Schema({
     type : Number,
     default : 0
   },
-  socketID : {type : String}
-})
+  socketID : {type : String},
+  isPartyLeader: {type : Boolean, default : false}, 
+  WPM : {type : Number, default: -1}, 
+  nickName : type : String}
+});
+
+const GameSchema = new mongoose.Schema({ 
+  words : [{type : String}], 
+  isOpen : {type : Boolean, default : true}, 
+  isOver : {type : Boolean, default : false}, 
+  players : [PlayerSchema], 
+  startTime: {type : Number}
+});
+
+module.exports = mongoose.model('Game', GameSchema);
