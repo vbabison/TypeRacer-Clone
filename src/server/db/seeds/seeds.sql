222e5c61abf955b393f78c7d0c7c14ccbf86d036
Babison Vilvarajah <officialbookingssf@gmail.com>
2021-06-08 13:08:02 -0400

    Fixing dependencies

--- src/server/db/seeds/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users (name, email, password)
-VALUES ('Bobby Brown', 'bobby@example.com', 'password');
-INSERT INTO users (name, email, password)
-VALUES ('Sergey Yesenin', 'sergey@example.com', 'password');
-INSERT INTO users (name, email, password)
-VALUES ('Nick Cage', 'nick@example.com', 'password');
-INSERT INTO users (name, email, password)
-VALUES ('John Dough', 'dough@example.com', 'password');
